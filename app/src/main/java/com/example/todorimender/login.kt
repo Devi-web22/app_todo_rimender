93d45d5406ff91824cf0d5588b2f4feb3393f919
Devi-web22 <nengdeviyuliyanti@gmail.com>
2025-09-23 10:54:39 +0700

    commit tanbah login

--- app/src/main/java/com/example/todorimender/login.kt
+++ app/src/main/java/com/example/todorimender/login.kt
@@ -1,36 +1,20 @@
 package com.example.todorimender
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class splash_screen : AppCompatActivity() {
-
-    private val splashDelayMillis: Long = 2000
-
+class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         enableEdgeToEdge()
-
-        setContentView(R.layout.activity_splash_screen)
-
-
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }, splashDelayMillis)
     }
 }
